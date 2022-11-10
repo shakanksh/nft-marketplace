bf63bb3319b764c4c576aa427f9e682b4001ee57
Shakanksh Sinha <shakankshs@gmail.com>
2022-11-10 11:50:00 +0530

    Post Design and Pulling NFT Details Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import Header from "../Components/Header";
 import styles from "../styles/Home.module.css";
+import {
+	useActiveListings,
+	useContract,
+	MediaRenderer,
+} from "@thirdweb-dev/react";
+import { ListingType } from "@thirdweb-dev/sdk";
 
 export default function Home() {
+	const { contract } = useContract(
+		process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT,
+		"marketplace"
+	);
+	const { data: listings, isLoading: loadingListings } =
+		useActiveListings(contract);
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -12,6 +25,50 @@ export default function Home() {
 			</Head>
 
 			<Header />
+			<main>
+				{loadingListings ? (
+					<div className={styles.loadingWrapper}>
+						<p className={styles.loading}>Loading...</p>
+					</div>
+				) : (
+					<div className={styles.listingWrapper}>
+						{listings?.map((listing) => (
+							<div className={styles.listing} key={listing.id}>
+								<div className={styles.mediaWrapper}>
+									<MediaRenderer
+										className={styles.media}
+										src={listing.asset.image}
+									/>
+								</div>
+
+								<div className={styles.infoWrapper}>
+									<div className={styles.info}>
+										<h2 className={styles.heading}>{listing.asset.name}</h2>
+										<p className={styles.description}>
+											{listing.asset.description}
+										</p>
+									</div>
+
+									<p className={styles.priceWrapper}>
+										<span className={styles.price}>
+											{listing.buyoutCurrencyValuePerToken.displayValue}
+										</span>
+										{listing.buyoutCurrencyValuePerToken.symbol}
+									</p>
+
+									<div className={styles.listingTypeWrapper}>
+										<p className={styles.listingType}>
+											{listing.type === ListingType.Direct
+												? "Buy Now"
+												: "Bid Now"}
+										</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				)}
+			</main>
 		</div>
 	);
 }
